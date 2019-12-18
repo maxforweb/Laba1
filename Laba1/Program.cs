26d62b62d4e9779b774eeb2b4fe1e4a80771695d
МАКС-ПК\Макс <maxkapustian@gmai.com>
2019-12-18 13:09:35 +0200

    Code add

--- Laba1/Program.cs
+++ Laba1/Program.cs
@@ -8,8 +8,22 @@ namespace Laba1
 {
     class Program
     {
+        static int x;
+        static int y;
         static void Main(string[] args)
         {
+            Console.WriteLine("Введите X");
+            x = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите Y");
+            y = Convert.ToInt32(Console.ReadLine());
+
+            int z = x + y;
+            Console.WriteLine($"Сумма введенных значений равна: {z} ");
+
+            Console.ReadKey();
         }
     }
+
+
 }
